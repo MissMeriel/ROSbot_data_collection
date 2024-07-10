7b00d405dc9c3846140d2a6dc67348075b8f3f64
zachkr05 <zachkr05@gmail.com>
2024-07-09 21:14:52 -0400

    minor error in print statement causing key error

--- training/Zach_train_DAVE2.py
+++ training/Zach_train_DAVE2.py
@@ -101,7 +101,7 @@ def main():
             #chatgpt
             # Changed: Access sample directly from batch
             for sample in batch:
-                print(sample['image'].shape)
+                print(sample['image name'].shape)
                 batch_images.append(sample['image name '])  # Append image to batch_images list
                 batch_labels.append(sample['angular_speed_z'])  # Append angular speed to batch_labels list
 
