16afbdb798664e369e2df77384bafabbb5bc825b
Meriel <meriel.k.stein@gmail.com>
2024-09-27 12:22:03 -0400

    added training scripts for MiniRNN

---
+++ training/DatasetGenerator_Recurrent.py
@@ -0,0 +1,264 @@
+import numpy as np
+import os, cv2, csv
+# from DAVE2 import DAVE2Model
+# from DAVE2pytorch import DAVE2PytorchModel
+import kornia
+import torchvision
+from torchvision.transforms import Compose, ToTensor, PILToTensor, functional as transforms
+from PIL import Image
+import copy
+from scipy import stats
+import torch.utils.data as data
+from pathlib import Path
+import skimage.io as sio
+import pandas as pd
+import torch
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import imshow
+import random
+
+
+def stripleftchars(s):
+    # print(f"{s=}")
+    for i in range(len(s)):
+        if s[i].isnumeric():
+            return s[i:]
+    return -1
+
+class DataSequence(data.Dataset):
+    def __init__(self, root, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root = root
+        self.transform = transform
+
+        image_paths = []
+        for p in Path(root).iterdir():
+            if p.suffix.lower() in [".jpg", ".png", ".jpeg", ".bmp"]:
+                image_paths.append(p)
+        image_paths.sort(key=lambda p: int(stripleftchars(p.stem)))
+        self.image_paths = image_paths
+        # print(f"{self.image_paths=}")
+        self.df = pd.read_csv(f"{self.root}/data_cleaned.csv")
+        self.cache = {}
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        if idx in self.cache:
+            return self.cache[idx]
+        img_name = self.image_paths[idx]
+        image = sio.imread(img_name)
+
+        df_index = self.df.index[self.df['image name'] == img_name.name]
+        y_thro = self.df.loc[df_index, 'linear_speed_x'].array[0]
+        y_steer = self.df.loc[df_index, 'angular_speed_z'].array[0]
+        y = [y_steer, y_thro]
+        # torch.stack(y, dim=1)
+        y = torch.tensor(y_steer)
+
+        # plt.title(f"steering_input={y_steer.array[0]}")
+        # plt.imshow(image)
+        # plt.show()
+        # plt.pause(0.01)
+
+        if self.transform:
+            image = self.transform(image).float()
+        # print(f"{img_name.name=} {y_steer=}")
+        # print(f"{image=}")
+        # print(f"{type(image)=}")
+        # print(self.df)
+        # print(y_steer.array[0])
+
+        # sample = {"image": image, "steering_input": y_steer.array[0]}
+        sample = {"image name": image, "angular_speed_z": y}
+
+        self.cache[idx] = sample
+        return sample
+
+class MultiDirectoryDataSequence(data.Dataset):
+    def __init__(self, root, image_size=(100,100), transform=None, robustification=False, noise_level=10, recurrence_length=4):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root = root
+        self.transform = transform
+        self.size = 0
+        self.recurrence_length = recurrence_length
+        self.image_size = image_size
+        image_paths_hashmap = {}
+        all_image_paths = []
+        self.dfs_hashmap = {}
+        self.dirs = []
+        marker = "collection"
+        for p in Path(root).iterdir():
+            if p.is_dir() and marker in str(p):
+                self.dirs.append("{}/{}".format(p.parent,p.stem.replace(marker, "")))
+                image_paths = []
+                # print(f"testing testing testing!")
+                try:
+                    self.dfs_hashmap[f"{p}"] = pd.read_csv(f"{p}/data_cleaned.csv")
+                    # add to debug
+                    print(f"Found data_cleaned.csv in {p.parent}/{p.stem.replace(marker, '')}")
+                except FileNotFoundError as e:
+                    print(f"{e} \nNo data_cleaned.csv in directory {p.parent}/{p.stem.replace(marker, '')}")
+                    continue
+                for pp in Path(p).iterdir():
+                    if pp.suffix.lower() in [".jpg", ".png", ".jpeg", ".bmp"] and "collection_trajectory" not in pp.name:
+                        image_paths.append(pp)
+                        all_image_paths.append(pp)
+                if image_paths:
+                    image_paths.sort(key=lambda p: int(stripleftchars(p.stem)))
+                    image_paths_hashmap[p] = copy.deepcopy(image_paths)
+                    self.size += len(image_paths)
+                else:
+                    print(f"No valid images found in directory: {p.parent}/{p.stem.replace(marker, '')}")
+        print("Finished intaking image paths!")
+        self.image_paths_hashmap = image_paths_hashmap
+        self.all_image_paths = all_image_paths
+        # self.df = pd.read_csv(f"{self.root}/data_cleaned.csv")
+        self.cache = {}
+        self.robustification = robustification
+        self.noise_level = noise_level
+        # random.seed(123)
+
+    def get_total_samples(self):
+        return self.size
+
+    def get_directories(self):
+        return self.dirs
+
+    def __len__(self):
+        return len(self.all_image_paths)
+
+
+    def robustify(self, y_steer, image, recurrent_vec):
+        if random.random() > 0.5:
+            # flip image
+            image = torch.flip(image, (2,))
+            y_steer = -y_steer
+            recurrent_vec = -recurrent_vec
+        if random.random() > 0.5:
+            # blur
+            gauss = kornia.filters.GaussianBlur2d((3,3), (1.5, 1.5))
+            # gauss = kornia.filters.GaussianBlur2d((5, 5), (5.5, 5.5))
+            # image = gauss(image[None])[0]
+            # image = kornia.filters.blur_pool2d(image[None], 3)[0]
+            # image = kornia.filters.max_blur_pool2d(image[None], 3, ceil_mode=True)[0]
+            # image = kornia.filters.median_blur(image, (3, 3))
+            # image = kornia.filters.median_blur(image, (10, 10))
+            # image = kornia.filters.box_blur(image, (3, 3))
+            # image = kornia.filters.box_blur(image, (5, 5))
+            # image = kornia.resize(image, image.shape[2:])
+            # plt.imshow(image.permute(1,2,0))
+            # plt.pause(0.01)
+            image = gauss(image[None])[0]
+        if random.random() > 0.5:
+            # darkness
+            contrast_factor = torch.rand(1).item() * 10 + 0.1
+            image = torchvision.transforms.functional.adjust_brightness(image, contrast_factor)
+        if random.random() > 0.5:
+            # contrast
+            # 0 gives a solid gray image, 1 gives the original image while 2 increases the contrast by a factor of 2.
+            contrast_factor = torch.rand(1).item() * 10 + 0.1
+            image = torchvision.transforms.functional.adjust_contrast(image, contrast_factor)
+        # noise
+        image = torch.clamp(image + (torch.randn(*image.shape) / self.noise_level), 0, 1)
+        return y_steer, image, recurrent_vec
+
+    def __getitem__(self, idx):
+        if idx in self.cache:
+            if self.robustification:
+                sample = self.cache[idx]
+                y_steer = sample["angular_speed_z"]
+                image = copy.deepcopy(sample["image name"])
+                recurrent_vec = copy.deepcopy(sample["recurrent_vec"])
+                y_steer, image, recurrent_vec = self.robustify(y_steer, image)
+                return {"image name": image, "angular_speed_z": y_steer, "linear_speed_x": sample["linear_speed_x"], "recurrent_vec": recurrent_vec}
+            else:
+                return self.cache[idx]
+        img_name = self.all_image_paths[idx]
+        image = Image.open(img_name)
+        image = image.resize(self.image_size)
+        orig_image = self.transform(image)
+        pathobj = Path(img_name)
+        df = self.dfs_hashmap[f"{pathobj.parent}"]
+        df_index = df.index[df['image name'] == img_name.name]
+
+        # Check if df_index is empty or has more than one entry
+        if len(df_index) != 1:
+            # Print all items in df_index
+            print(f"All items in Error df_index: {df_index.tolist()}")
+            raise ValueError(f"Expected exactly one row for {img_name.name}, found {len(df_index)} rows (df_index) in directory: {self.root}. df is {df}. pathobj is {pathobj}")
+
+        orig_y_steer = df.loc[df_index, 'angular_speed_z'].item()
+        y_throttle = df.loc[df_index, 'linear_speed_x'].item()
+
+        # get recurrent vector
+        orig_recurrent_vec = torch.zeros((1, self.recurrence_length), dtype=torch.float32)
+        for i in range(self.recurrence_length):
+            try:
+                orig_recurrent_vec[0][i] = df.loc[df_index-i-1, 'angular_speed_z'].item()
+            except Exception as e:
+                pass
+                # print(e)
+                # print(f"{df_index=}")
+                # print(f"{orig_y_steer=:.3f}    \t{orig_recurrent_vec=}")
+
+        y_steer = copy.deepcopy(orig_y_steer)
+        recurrent_vec = torch.clone(orig_recurrent_vec)
+        if self.robustification:
+            image = copy.deepcopy(orig_image)
+            y_steer, image, recurrent_vec = self.robustify(y_steer, image, recurrent_vec)
+        else:
+            t = Compose([ToTensor()])
+            image = t(image).float()
+            # image = torch.from_numpy(image).permute(2,0,1) / 127.5 - 1
+
+        # vvvvvv uncomment below for value-image debugging vvvvvv
+        # plt.title(f"{img_name}\nsteering_input={y_steer.array[0]}", fontsize=7)
+        # plt.imshow(image)
+        # plt.show()
+        # plt.pause(0.01)
+
+        sample = {"image": img_name.name, "image name": image, "angular_speed_z": torch.FloatTensor([y_steer]), "linear_speed_x": torch.FloatTensor([y_throttle]), "recurrent_vec": recurrent_vec}
+        orig_sample = {"image name": orig_image, "angular_speed_z": torch.FloatTensor([orig_y_steer]), "linear_speed_x": torch.FloatTensor([y_throttle]), "recurrent_vec": orig_recurrent_vec}
+        # self.cache[idx] = orig_sample
+        return sample
+
+    def get_outputs_distribution(self):
+        all_outputs = np.array([])
+        for key in self.dfs_hashmap.keys():
+            df = self.dfs_hashmap[key]
+            arr = df['angular_speed_z'].to_numpy()
+            # print("len(arr)=", len(arr))
+            all_outputs = np.concatenate((all_outputs, arr), axis=0)
+            # print(f"Retrieved dataframe {key=}")
+        all_outputs = np.array(all_outputs)
+        moments = self.get_distribution_moments(all_outputs)
+        return moments
+
+    ##################################################
+    # ANALYSIS METHODS
+    ##################################################
+
+    # Moments are 1=mean 2=variance 3=skewness, 4=kurtosis
+    def get_distribution_moments(self, arr):
+        moments = {}
+        moments['shape'] = np.asarray(arr).shape
+        moments['mean'] = np.mean(arr)
+        moments['median'] = np.median(arr)
+        moments['var'] = np.var(arr)
+        moments['skew'] = stats.skew(arr)
+        moments['kurtosis'] = stats.kurtosis(arr)
+        moments['max'] = max(arr)
+        moments['min'] = min(arr)
+        return moments
---
+++ training/train_MiniRecurrent.py
@@ -0,0 +1,176 @@
+import numpy as np
+import argparse
+import pandas as pd
+import matplotlib.image as mpimg
+from torch.autograd import Variable
+
+# import h5py
+import os
+# from PIL import Image
+# import PIL
+import matplotlib.pyplot as plt
+# import csv
+from DatasetGenerator_Recurrent import MultiDirectoryDataSequence
+import time
+import sys
+sys.path.append("../models")
+from DAVE2pytorch import *
+from MiniRecurrent import *
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from torch.utils import data
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from torchvision.transforms import Compose, ToPILImage, ToTensor, Resize, Lambda, Normalize
+from utils import *
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', type=str, help='parent directory of training dataset')
+    parser.add_argument("--batch", type=int, default=64)
+    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--robustification", action='store_true')
+    parser.add_argument("--convergence", action='store_true')
+    parser.add_argument("--normalize", action='store_true')
+    parser.add_argument("--noisevar", type=int, default=15)
+    parser.add_argument("--recurrence_length", type=int, default=4)
+    parser.add_argument("--log_interval", type=int, default=50)
+    parser.add_argument("--slurmid", type=int, default=0)
+    parser.add_argument("--archid", type=str, default="MiniRNN")
+    parser.add_argument("--lossfxn", type=str, default="MSE")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    print(args)
+    start_time = time.time()
+    input_shape = (2560, 720)  # Example input shape: width x height
+    if args.archid == "DAVE2v1":
+        model = DAVE2v1(input_shape=input_shape)
+    elif args.archid == "DAVE2v2":
+        model = DAVE2v2(input_shape=input_shape)
+    elif args.archid == "DAVE2v3":
+        model = DAVE2v3(input_shape=input_shape)
+    elif args.archid == "DAVE2v3Norm":
+        model = DAVE2v3Norm(input_shape=input_shape)
+    elif args.archid == "Epoch":
+        model = Epoch(input_shape=input_shape)
+    elif args.archid == "MiniRNN":
+        # rnn_type, ntoken, ninp, nhid, nlayers
+        model = MiniRNN(input_shape=input_shape, rnn_hidden_size=args.recurrence_length, nlayers=args.batch, dropout=0.5, tie_weights=False)
+
+    if args.normalize:
+        transform = Compose([ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    else:
+        transform = Compose([ToTensor()])
+    dataset = MultiDirectoryDataSequence(args.dataset, image_size=(model.input_shape[::-1]), transform=transform,\
+                                         robustification=args.robustification, noise_level=args.noisevar, recurrence_length=args.recurrence_length)
+    print("Retrieving output distribution....")
+    print("Moments of distribution:", dataset.get_outputs_distribution())
+    print("Total samples:", dataset.get_total_samples(), flush=True)
+    def worker_init_fn(worker_id):
+        np.random.seed(np.random.get_state()[1][0] + worker_id)
+
+    trainloader = DataLoader(dataset, batch_size=args.batch, shuffle=False, worker_init_fn=worker_init_fn)
+    print(f"Time to load dataset: {(time.time() - start_time):.1f}s", flush=True)
+    outdir = f"./training-output/{model._get_name()}-NOSHUFFLE-{randstr()}-{timestr()}-{args.slurmid}/"
+    os.makedirs(outdir, exist_ok=True)
+    iteration = f'{model._get_name()}-NOSHUFFLE-{input_shape[0]}x{input_shape[1]}-loss{args.lossfxn}-aug{args.robustification}-norm{args.normalize}-{args.epochs}epoch-{args.batch}batch-{int(dataset.get_total_samples()/1000)}Ksamples'
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"{iteration=}")
+    print(f"{device=}")
+    model.train()
+    model = model.to(device)
+    # if loss doesnt level out after 20 epochs, either inc epochs or inc learning rate
+    optimizer = optim.Adam(model.parameters(), lr=args.lr) #, betas=(0.9, 0.999), eps=1e-08)
+    lowest_loss = 1e5
+    logfreq = 20
+    if args.lossfxn == "L1":
+        lossfxn = nn.L1Loss()
+    else:
+        lossfxn = nn.MSELoss()
+    for epoch in range(args.epochs):
+        epoch_start_time = time.time() # Record start time for the epoch
+        running_loss = 0.0
+        sampled_loss = np.ones(10)
+        for i, hashmap in enumerate(trainloader, 0):
+            x = hashmap['image name'].float().to(device)
+            y = hashmap['angular_speed_z'].float().to(device)
+            current_rvec = hashmap['recurrent_vec'][0].float().to(device)
+            x = Variable(x, requires_grad=True)
+            y = Variable(y, requires_grad=False)
+            current_rvec = Variable(current_rvec, requires_grad=True)
+            # print(f"Batch {i} {current_rvec.shape=}")
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs, rvec = model(x, current_rvec)
+            loss = lossfxn(outputs, y) #+ lossfxn(current_rvec, rvec)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % logfreq == logfreq-1:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.7f' %
+                      (epoch + 1, i + 1, running_loss / logfreq))
+                sampled_loss = np.roll(sampled_loss, 1)
+                sampled_loss[0] = running_loss / logfreq
+                if epoch > 5 and (running_loss / logfreq) < lowest_loss:
+                    print(f"New best model! {args.lossfxn} Loss: {running_loss / logfreq}", flush=True)
+                    model_name = f"{outdir}/model-{iteration}-best.pt"
+                    print(f"Saving model to {model_name}", flush=True)
+                    torch.save(model, model_name)
+                    lowest_loss = running_loss / logfreq
+                running_loss = 0.0
+        epoch_end_time = time.time()  # Record end time for the epoch
+        epoch_duration = epoch_end_time - epoch_start_time # Record total time for the epoch
+        print(f"Finished Epoch {epoch + 1}\nEpoch Duration: {epoch_duration:.1f}s", flush=True)
+        sys.stdout.flush()
+        model_name = f"{outdir}/model-{iteration}-epoch{epoch}.pt"
+        print(f"Saving model to {model_name}")
+        torch.save(model, model_name)
+        print(f"{np.var(sampled_loss)=}", flush=True)
+        if args.convergence and np.var(sampled_loss) < 0.0001:
+            print(f"Loss converged at {loss} (variance {np.var(sampled_loss):.4f}); quitting training...", flush=True)
+            break
+    print('Finished Training')
+
+    # save model
+    # torch.save(model.state_dict(), f'H:/GitHub/DAVE2-Keras/test{iteration}-weights.pt')
+    model_name = f'{outdir}/model-{iteration}.pt'
+    torch.save(model.state_dict(), model_name)
+
+    # delete models from previous epochs
+    # print("Deleting models from previous epochs...")
+    # for epoch in range(args.epochs):
+    #     os.remove(f"{outdir}/model-{iteration}-epoch{epoch}.pt")
+    print(f"Saving model to {model_name}")
+    print("All done :)")
+    time_to_train=time.time() - start_time
+    print("Time to train: {}".format(time_to_train))
+    # save metainformation about training
+    with open(f'./model-{iteration}-metainfo.txt', "w") as f:
+        f.write(f"{model_name=}\n"
+                f"total_samples={dataset.get_total_samples()}\n"
+                f"{args.epochs=}\n"
+                f"{args.lr=}\n"
+                f"{args.batch=}\n"
+                f"{optimizer=}\n"
+                f"final_loss={running_loss / logfreq}\n"
+                f"{device=}\n"
+                f"{args.robustification=}\n"
+                f"{args.noisevar=}\n"
+                f"dataset_moments={dataset.get_outputs_distribution()}\n"
+                f"{time_to_train=}\n"
+                f"dirs={dataset.get_directories()}")
+
+
+if __name__ == '__main__':
+    main()
