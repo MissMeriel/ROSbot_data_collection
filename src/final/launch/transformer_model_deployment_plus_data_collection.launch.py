a7514b3093c9b0d214d782a63aae3c816ef8fb6d
Yiliii <yililee233@gmail.com>
2024-10-04 01:34:37 -0400

    Added deployment plus data collection launch file for the onnx transformer models

---
+++ src/final/launch/transformer_model_deployment_plus_data_collection.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import TextSubstitution
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    dest_launch_arg = DeclareLaunchArgument("dest", default_value=TextSubstitution(text="/home/husarion/media/usb9_23_onnxtransformer"))
+	
+    return LaunchDescription([
+    	dest_launch_arg,
+    	IncludeLaunchDescription(
+    	    PythonLaunchDescriptionSource([
+    	        PathJoinSubstitution([
+    	            FindPackageShare('rplidar_ros'),
+    	            'launch',
+    	            'view_rplidar_a3_launch.py'
+    	        ])
+    	    ]),
+    	    launch_arguments={}.items()
+    	),
+        Node(
+            package='joy',
+            namespace='',
+            executable='joy_node',
+            name='joy'
+        ),
+        Node(
+            package='final',
+            executable='onnx_steering_NN_transformer',
+            name='sim',
+            parameters=[
+            	{"dest": LaunchConfiguration("dest")},
+            ],
+            output='screen',
+            emulate_tty=True
+        ),
+        Node(
+            package='final',
+            executable='ros2_data_collection',
+            name='sim',
+            parameters=[
+            	{"dest": LaunchConfiguration("dest")},
+            ],
+            output='screen',
+            emulate_tty=True
+        ),
+        Node(
+            package='usb_cam',
+            namespace ='',
+            executable='usb_cam_node_exe',
+            arguments=['--ros-args', '--params-file', '/home/husarion/.ros/camera_info/default_cam.yaml']
+            #parameters=[{'params-file': '/home/husarion/.ros/camera_info/default_cam.yaml'}]
+        )
+    ])
