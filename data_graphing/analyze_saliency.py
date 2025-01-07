b41d9f7f395ce10fbea43ce9c69f27a1e7318f62
Meriel <meriel.k.stein@gmail.com>
2025-01-06 21:35:24 -0500

    added saliency analysis script

--- data_graphing/analyze_saliency.py
+++ data_graphing/analyze_saliency.py
@@ -1,6 +1,6 @@
 import sys
+# sys.path.append("C:/Users/Meriel/Documents/GitHub/contextualvalidation")
 sys.path.append("../models")
-sys.path.append( "../training")
 from DAVE2pytorch import *
 import numpy as np
 import PIL.Image
@@ -16,6 +16,9 @@ import pandas as pd
 import random, string
 import cv2
 import time
+import sys
+sys.path.append("../models/")
+sys.path.append( "../training")
 from torch.autograd import Variable
 from DatasetGenerator import *
 import torch.nn.functional as F
@@ -26,10 +29,11 @@ from torchmetrics.image import VisualInformationFidelity
 from skimage.metrics import structural_similarity as ssim
 import pickle
 import argparse
+from tqdm import tqdm
 
 def parse_args():
     parser = argparse.ArgumentParser(
-                    prog='XRAISaliencyComparison',
+                    prog='XRAISaliencyAnalysis',
                     description='',
                     epilog='Generating XRAIs is computationally expensive so we generate them separately and then unpickle them for use in analyze_saliency.py')
     parser.add_argument('-w', '--modelweights', type=str, default="/p/rosbot/rosbotxl/models-yili/M1/model-DAVE2v3-2560x720-lr0.0001-100epoch-64batch-lossMSE-11Ksamples-INDUSTRIALandHIROCHIandUTAH-noiseflipblur.pt")
@@ -119,9 +123,9 @@ def parse_range(trace, rng):
     idx_end = [i for i in range(len(trace)) if trace[i] == rng.split(",")[1]+".pickle"][0]
     return idx_start, idx_end
 
-from tqdm import tqdm
+
 def main(args):
-    outdir = f"./results-xrai-comparison2/"
+    outdir = f"./results-xrai-comparison/"
     os.makedirs(outdir, exist_ok=True)
     passingtrace = os.listdir(args.passingxrais)
     passingtrace.sort()
@@ -138,55 +142,49 @@ def main(args):
     elif len(passingtrace) < len(failingtrace):
         diff = len(failingtrace) - len(passingtrace)
         failingtrace = failingtrace[:-diff]
-    prediction_threshold = 1e-3
-    heatmap_variance_threshold = 1e-8
     for i, pair in tqdm(enumerate(zip(passingtrace, failingtrace))):
-        # print(f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}")
         # load in each XRAI
-        # dict_keys(['image', 'image_name', 'xrai', 'prediction', 'steering_input'])
         pass_hashmap = unpickle(args.passingxrais + pair[0])
         fail_hashmap = unpickle(args.failingxrais + pair[1])
-        pass_prediction = pass_hashmap['prediction'].item()
-        fail_prediction = fail_hashmap['prediction'].item()
+        # dict_keys(['image', 'image_name', 'xrai', 'prediction', 'steering_input'])
+        ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
+        P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
+        id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-plain"
+        ShowImage(pass_hashmap['image'], title=f"COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f})", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
+        ShowImage(fail_hashmap['image'], title=f"FAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f})", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
+         
+        # normalize heatmap scales
+        normalization_max = max(np.max(pass_hashmap['xrai']), np.max(fail_hashmap['xrai']))
+        normalization_min = min(np.min(pass_hashmap['xrai']), np.min(fail_hashmap['xrai']))
+        pass_hashmap['xrai'][0][0] = normalization_max
+        pass_hashmap['xrai'][0][1] = normalization_min
+        fail_hashmap['xrai'][0][0] = normalization_max
+        fail_hashmap['xrai'][0][1] = normalization_min
+
+        # compare plain heatmaps
+        ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
+        P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
+        id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}"
+        ShowHeatMap(pass_hashmap['xrai'], f"PASS{pair[0].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
+        ShowHeatMap(fail_hashmap['xrai'], f"FAIL{pair[1].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
+
+        # write overlaid normalized heatmaps
+        ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
+        P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
+        colormap = plt.get_cmap('inferno')
+        pass_heatmap = colormap(pass_hashmap['xrai'] / (normalization_max - normalization_min))
+        pass_heatmap = np.delete(pass_heatmap, 3, 2) 
+        fail_heatmap = colormap(fail_hashmap['xrai'] / (normalization_max - normalization_min))
+        fail_heatmap = np.delete(fail_heatmap, 3, 2) 
+        id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}"
+        ShowOverlay(pass_hashmap['image'], pass_heatmap.astype(np.float32), f"PASS{pair[0].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
+        ShowOverlay(fail_hashmap['image'], fail_heatmap.astype(np.float32), f"FAIL{pair[1].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
+        
+        # compare diff'ed heatmaps
         pass_variance = np.var(pass_hashmap['xrai'])
         fail_variance = np.var(fail_hashmap['xrai'])
-        if abs(pass_prediction - fail_prediction) < prediction_threshold:
-            ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
-            P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
-            id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-plain"
-            ShowImage(pass_hashmap['image'], title=f"COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f})", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
-            ShowImage(fail_hashmap['image'], title=f"FAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f})", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
-            
-            # normalize heatmap scales
-            normalization_max = max(np.max(pass_hashmap['xrai']), np.max(fail_hashmap['xrai']))
-            normalization_min = min(np.min(pass_hashmap['xrai']), np.min(fail_hashmap['xrai']))
-            pass_hashmap['xrai'][0][0] = normalization_max
-            pass_hashmap['xrai'][0][1] = normalization_min
-            fail_hashmap['xrai'][0][0] = normalization_max
-            fail_hashmap['xrai'][0][1] = normalization_min
-
-            # compare plain heatmaps
-            ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
-            P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
-            id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}"
-            ShowHeatMap(pass_hashmap['xrai'], f"PASS{pair[0].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
-            ShowHeatMap(fail_hashmap['xrai'], f"FAIL{pair[1].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
-
-            # write overlaid normalized heatmaps
-            ROWS, COLS, UPSCALE_FACTOR = 2, 1, 10
-            P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
-            colormap = plt.get_cmap('inferno')
-            pass_heatmap = colormap(pass_hashmap['xrai'] / (normalization_max - normalization_min))
-            pass_heatmap = np.delete(pass_heatmap, 3, 2) 
-            fail_heatmap = colormap(fail_hashmap['xrai'] / (normalization_max - normalization_min))
-            fail_heatmap = np.delete(fail_heatmap, 3, 2) 
-            id = f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}"
-            ShowOverlay(pass_hashmap['image'], pass_heatmap.astype(np.float32), f"PASS{pair[0].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 1), outfile=outdir, id=id)
-            ShowOverlay(fail_hashmap['image'], fail_heatmap.astype(np.float32), f"FAIL{pair[1].replace('.pickle', '')}", ax=P.subplot(ROWS, COLS, 2), outfile=outdir, id=id)
-            
-            # compare diff'ed heatmaps
-            ShowHeatMap(abs(pass_hashmap['xrai'] - fail_hashmap['xrai']), f"ABS. COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f} var {pass_variance})\nFAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f} var {fail_variance})", outfile=outdir, id=f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-abscomparison")
-            ShowHeatMap(pass_hashmap['xrai'] - fail_hashmap['xrai'], f"SIGNED COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f} var {pass_variance})\nFAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f} var {fail_variance})", outfile=outdir, id=f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-comparison")
+        ShowHeatMap(abs(pass_hashmap['xrai'] - fail_hashmap['xrai']), f"ABS. COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f} var {pass_variance})\nFAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f} var {fail_variance})", outfile=outdir, id=f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-abscomparison")
+        ShowHeatMap(pass_hashmap['xrai'] - fail_hashmap['xrai'], f"SIGNED COMPARISON \nPASS{pair[0].replace('.pickle', '')} (pred {pass_hashmap['prediction'].item():.3f} var {pass_variance})\nFAIL{pair[1].replace('.pickle', '')} (pred {fail_hashmap['prediction'].item():.3f} var {fail_variance})", outfile=outdir, id=f"PASS{pair[0].replace('.pickle', '')}-FAIL{pair[1].replace('.pickle', '')}-comparison")
 
 if __name__ == '__main__':
     args = parse_args()
