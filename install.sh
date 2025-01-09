01656016b05a07f248cced347fd128c017d6014c
Meriel <meriel.k.stein@gmail.com>
2025-01-09 13:58:12 -0500

    updated installation to include catalog download

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ python3.10 venv .venv-rb
 . .venv-rb/bin/activate
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python -m pip install -r requirements.txt
-mkdir ./datasets
-echo "Download and unzip data to ./datasets"
+# download catalog
+./download.sh ALL
